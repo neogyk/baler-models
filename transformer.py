3f3065bf67d440de55949048093482d74918de73
Leonid Didukh <ledidukh@gmail.com>
2024-05-20 23:58:23 +0300

    transformer AE

---
+++ transformer.py
@@ -0,0 +1,147 @@
+import torch
+from torch import nn
+import pdb
+
+
+class TransformerAE(nn.Module):
+    """_summary_
+
+    Args:
+        nn (_type_): _description_
+    """
+
+    def __init__(
+        self,
+        in_dim,
+        h_dim=256,
+        n_heads=1,
+        latent_size=50,
+        activation=torch.nn.functional.gelu,
+    ):
+        super(TransformerAE, self).__init__()
+
+        # self.pos_encoder = PositionalEncoding(d_model=1, max_len=806)
+
+        self.transformer_encoder_layer_1 = torch.nn.TransformerEncoderLayer(
+            batch_first=True,
+            d_model=in_dim,
+            activation=activation,
+            dim_feedforward=h_dim,
+            nhead=n_heads,
+        )
+
+        self.transformer_encoder_layer_2 = torch.nn.TransformerEncoderLayer(
+            batch_first=True,
+            d_model=256,
+            activation=activation,
+            dim_feedforward=256,
+            nhead=n_heads,
+        )
+        self.transformer_encoder_layer_3 = torch.nn.TransformerEncoderLayer(
+            batch_first=True,
+            d_model=128,
+            activation=activation,
+            dim_feedforward=128,
+            nhead=n_heads,
+        )
+
+        self.encoder_layer_1 = torch.nn.Sequential(
+            torch.nn.LazyBatchNorm1d(),
+            torch.nn.Linear(in_dim, 256),
+        )
+        self.encoder_layer_2 = torch.nn.Sequential(
+            torch.nn.LeakyReLU(),
+            torch.nn.LazyBatchNorm1d(),
+            torch.nn.Linear(256, 128),
+            torch.nn.LeakyReLU(),
+        )
+
+        self.encoder_layer_3 = torch.nn.Sequential(
+            torch.nn.LazyBatchNorm1d(),
+            torch.nn.Linear(128, latent_size),
+            torch.nn.LeakyReLU(),
+        )
+
+        self.decoder_layer_3 = torch.nn.Sequential(
+            torch.nn.LazyBatchNorm1d(),
+            torch.nn.Linear(latent_size, 128),
+            torch.nn.LeakyReLU(),
+        )
+        self.decoder_layer_2 = torch.nn.Sequential(
+            torch.nn.LazyBatchNorm1d(), torch.nn.Linear(128, 256), torch.nn.LeakyReLU()
+        )
+        self.decoder_layer_1 = torch.nn.Sequential(
+            torch.nn.LazyBatchNorm1d(),
+            torch.nn.Linear(256, in_dim),
+            torch.nn.LeakyReLU(),
+        )
+
+        self.transformer_decoder_layer_1 = torch.nn.TransformerEncoderLayer(
+            d_model=in_dim,
+            dim_feedforward=h_dim,
+            activation=torch.nn.functional.gelu,
+            nhead=n_heads,
+        )
+
+        self.transformer_decoder_layer_2 = torch.nn.TransformerEncoderLayer(
+            batch_first=True,
+            d_model=256,
+            activation=activation,
+            dim_feedforward=256,
+            nhead=n_heads,
+        )
+        self.transformer_decoder_layer_3 = torch.nn.TransformerEncoderLayer(
+            batch_first=True,
+            d_model=128,
+            activation=activation,
+            dim_feedforward=128,
+            nhead=n_heads,
+        )
+
+    def encoder(self, x: torch.Tensor):
+        """_summary_
+
+        Args:
+            x (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        z = self.transformer_encoder_layer_1(x)
+        z = self.encoder_layer_1(z)
+        z = self.transformer_encoder_layer_2(z)
+        z = self.encoder_layer_2(z)
+        z = self.transformer_encoder_layer_3(z)
+        z = self.encoder_layer_3(z)
+
+        return z
+
+    def decoder(self, z: torch.Tensor):
+        """_summary_
+
+        Args:
+            z (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        x = self.decoder_layer_3(z)
+        x = self.transformer_decoder_layer_3(x)
+        x = self.decoder_layer_2(x)
+        x = self.transformer_decoder_layer_2(x)
+        x = self.decoder_layer_1(x)
+        x = self.transformer_decoder_layer_1(x)
+        return x
+
+    def forward(self, x: torch.Tensor):
+        """_summary_
+
+        Args:
+            z (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        z = self.encoder(x)
+        x = self.decoder(z)
+        return x
