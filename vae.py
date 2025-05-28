de455459302ad2328565d538ebb00346246e06f6
Leonid Didukh <ledidukh@gmail.com>
2025-05-28 22:40:21 +0300

    modified vae for the bitback coding

--- vae.py
+++ vae.py
@@ -1,8 +1,33 @@
 import torch
 from gbn_layer import GBN
+from typing import Literal
+import pdb
+from torch.distributions import Normal, Bernoulli
+import utils
+import rans
+import numpy as np
+
+rng = np.random.RandomState(0)
+
+_LAYER_TYPES = Literal["dense", "convolutional"]
+_ARCHITECTURE_TYPES = Literal["forward", "unet"]
 
 
-class bVAE(torch.nn.Module):
+def beta_binomial_log_pdf(k, n, alpha, beta):
+    numer = (
+        lgamma(n + 1) + lgamma(k + alpha) + lgamma(n - k + beta) + lgamma(alpha + beta)
+    )
+    denom = (
+        lgamma(k + 1)
+        + lgamma(n - k + 1)
+        + lgamma(n + alpha + beta)
+        + lgamma(alpha)
+        + lgamma(beta)
+    )
+    return numer - denom
+
+
+class betaVAE(torch.nn.Module):
     """
     beta - Variational Autoencoder (VAE) class.
 
@@ -11,19 +36,34 @@ class bVAE(torch.nn.Module):
         latent_dim (int): Dimensionality of the latent space.
     """
 
-    def __init__(self, in_dim: int, latent_dim: int):
-        super(bVAE, self).__init__()
-        latent_dim = 64
-        self.hidden_dim = [1024, 512, 256, 256]
-        self.hidden_dim_decoder = [256, 256, 512, 1024]
+    def __init__(
+        self,
+        in_dim: int,
+        latent_dim: int,
+        layer_type: _LAYER_TYPES = "convolutional",
+        architecture_type: _ARCHITECTURE_TYPES = "forward",
+        bitback=True,
+    ):
+        super(betaVAE, self).__init__()
+        self.layer_type: _LAYER_TYPES = layer_type
+        self.architecture_type: _ARCHITECTURE_TYPES = architecture_type
+        self.bitback = bitback
+        self.hidden_dim = [1, 32, 1]
+        self.hidden_dim_decoder = [1, 32, 1]
         self.latent_dim = latent_dim
         self.output_dim = in_dim
-        self.mu_l = torch.nn.Linear(self.hidden_dim[-1], latent_dim)
-        self.var_l = torch.nn.Linear(self.hidden_dim[-1], latent_dim)
+        self.mu_l = torch.nn.Linear(latent_dim, latent_dim)
+        self.var_l = torch.nn.Linear(latent_dim, latent_dim)
 
         self.encoder_modules = torch.nn.ModuleList(
             [
-                torch.nn.Linear(in_dim, h_dim)
+                (
+                    torch.nn.Linear(in_dim, h_dim)
+                    if self.layer_type is "dense"
+                    else torch.nn.Conv2d(
+                        in_dim, out_channels=h_dim, kernel_size=3, stride=5, padding=0
+                    )
+                )
                 for in_dim, h_dim in zip(
                     [in_dim] + self.hidden_dim[:], self.hidden_dim[:]
                 )
@@ -31,28 +71,47 @@ class bVAE(torch.nn.Module):
         )
         self.encoder_norm = torch.nn.ModuleList(
             [
-                GBN(h_dim) for h_dim in self.hidden_dim[:]
-            ]  
+                (
+                    GBN(h_dim)
+                    if self.layer_type is "dense"
+                    else torch.nn.BatchNorm2d(h_dim)
+                )
+                for h_dim in self.hidden_dim[:]
+            ]
         )
-
 
         self.decoder_modules = torch.nn.ModuleList(
             [
-                torch.nn.Linear(in_dim, h_dim)
+                (
+                    torch.nn.Linear(in_dim, h_dim)
+                    if self.layer_type is "dense"
+                    else torch.nn.ConvTranspose2d(
+                        in_dim,
+                        h_dim,
+                        kernel_size=3,
+                        stride=5,
+                        padding=0,
+                        output_padding=0,
+                    )
+                )
                 for in_dim, h_dim in zip(
-                    [self.latent_dim] + self.hidden_dim_decoder[:],
-                    self.hidden_dim_decoder[:] + [self.output_dim],
+                    self.hidden_dim_decoder[:],
+                    self.hidden_dim_decoder[1:] + [self.output_dim],
                 )
             ]
         )
         self.decoder_norm = torch.nn.ModuleList(
             [
-                GBN(h_dim)
-                for h_dim in self.hidden_dim_decoder[:] + [self.output_dim]
+                (
+                    GBN(h_dim)
+                    if self.layer_type is "dense"
+                    else torch.nn.BatchNorm2d(h_dim)
+                )
+                for h_dim in self.hidden_dim_decoder[1:] + [self.output_dim]
             ]
         )
 
-    def encoder(self, x):
+    def encode(self, x):
         """
         Encodes the input data into the latent space.
 
@@ -65,9 +124,12 @@ class bVAE(torch.nn.Module):
         """
         for i in range(len(self.encoder_modules)):
             x = self.encoder_modules[i](x)
-
+            print("X: ", x.size())
             x = self.encoder_norm[i](x)
             x = torch.nn.functional.leaky_relu(x)
+
+        if self.layer_type == "convolutional":
+            x = torch.flatten(x, start_dim=1)
 
         mu, logvar = torch.nn.functional.leaky_relu(
             self.mu_l(x)
@@ -84,10 +146,11 @@ class bVAE(torch.nn.Module):
             torch.Tensor: Sampled data from the latent space.
         """
         std = torch.exp(0.5 * logvar)
-        eps = torch.rand_like(std)
-        return mu + eps * std
+        eps = torch.randn_like(std)
+        latent = eps * std + mu
+        return latent
 
-    def decoder(self, z):
+    def decode(self, z: torch.Tensor):
         """
         Decodes the data from the latent space to the original input space.
 
@@ -101,8 +164,22 @@ class bVAE(torch.nn.Module):
             z = self.decoder_modules[i](z)
             z = self.decoder_norm[i](z)
             z = torch.nn.functional.leaky_relu(z)
-        z = torch.nn.functional.tanh(z) 
+
+        if self.bitback:
+            z = torch.nn.functional.tanh(z)
+        else:
+            z = torch.nn.functional.sigmoid(z)
         return z
+
+    def loss(self, x):
+        mu, log_var = self.encode(x)
+        x = self.reparameterize(mu, logvar=log_var)
+        x_probs = self.encode(x)
+        dist = Bernoulli(x_probs)
+        l = torch.sum(dist.log_prob(x.view(-1, 1)), dim=1)
+        p_z = torch.sum(Normal(0, 1).log_prob(z), dim=1)
+        q_z = torch.sum(Normal(z_mu, z_std).log_prob(z), dim=1)
+        return -torch.mean(l + p_z - q_z) * np.log2(np.e)
 
     def forward(self, x):
         """
@@ -113,7 +190,33 @@ class bVAE(torch.nn.Module):
         Returns:
             VAEOutput: VAE output dataclass.
         """
-        mu, log_var = self.encoder(x)
-        z = self.reparameterize(mu, log_var)
-        x = self.decoder(z)
+        mu, logvar = self.encode(x)
+        self.mu, self.logvar = mu, logvar
+        z = self.reparameterize(mu, logvar)
+        x = self.decode(z)
         return x
+
+    def compress(self, x):
+        x = [_x.view(-1) for _x in x]
+        other_bits = rng.randint(low=1 << 16, high=1 << 31, size=20, dtype=np.uint32)
+        state = rans.unflatten(other_bits)
+
+        for _x in x:
+            state = utils.vae_append(state, _x)
+
+        compressed_message = rans.flatten(state)
+        return compressed_message
+
+    def decompress(self, z):
+        state = rans.unflatten(z)
+        state, x = utils.vae_pop(state)
+        return x
+
+
+if __name__ == "__main__":
+
+    vae = betaVAE(256, latent_dim=4, layer_type="dense")
+
+    # Test the compression:
+    x = torch.rand(32, 3, 256, 256)
+    compressed = vae.compress(x)
